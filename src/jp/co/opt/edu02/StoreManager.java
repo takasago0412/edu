45c13657aff73868c17771ccc1d83cad74967e90
takasago <y_takasago@opt.co.jp>
2016-04-26 19:14:10 +0900

    コード整形

--- src/jp/co/opt/edu02/StoreManager.java
+++ src/jp/co/opt/edu02/StoreManager.java
@@ -5,26 +5,26 @@ import java.util.List;
 
 import jp.co.opt.edu01.Store;
 
-public class StoreManager extends jp.co.opt.edu01.StoreManager{
-	public StoreManager(List<Store> stores){
+public class StoreManager extends jp.co.opt.edu01.StoreManager {
+	public StoreManager(List<Store> stores) {
 		super(stores);
 	}
-	
+
 	/**
 	 * 店舗エリアごとに売上高を合計した店舗リストを返却する
 	 * @return 店舗エリアごとの売上高合計済み店舗リスト
 	 */
 	public List<Store> groupByArea() {
 		List<Store> sortStoreList = sortByArea();
-		List<Store> groupByAreaStore= new ArrayList<Store>();
+		List<Store> groupByAreaStore = new ArrayList<Store>();
 		int sum = 0;
-		String area;		
-		
+		String area;
+
 		sum = sortStoreList.get(0).getSales();
 		area = sortStoreList.get(0).getArea();
-		for(int i=1; i < sortStoreList.size() ; i++){
-			Store setStore= new Store();
-			if(area.equals(sortStoreList.get(i).getArea())){
+		for (int i = 1; i < sortStoreList.size(); i++) {
+			Store setStore = new Store();
+			if (area.equals(sortStoreList.get(i).getArea())) {
 				sum = sum + sortStoreList.get(i).getSales();
 			}
 			else {
@@ -35,12 +35,12 @@ public class StoreManager extends jp.co.opt.edu01.StoreManager{
 				sum = sortStoreList.get(i).getSales();
 				area = sortStoreList.get(i).getArea();
 			}
-			if(i == sortStoreList.size()-1){
-				setStore= new Store();
+			if (i == sortStoreList.size() - 1) {
+				setStore = new Store();
 				setStore.setArea(area);
 				setStore.setSales(sum);
 				setStore.setName("test");
-				groupByAreaStore.add(setStore);				
+				groupByAreaStore.add(setStore);
 			}
 		}
 		//TODO: 店舗エリアごとの売上高合計処理を実装(Mapやそのサブクラスを使用せずソートされた店舗リストを集計)
